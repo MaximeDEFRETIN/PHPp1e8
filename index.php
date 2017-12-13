af8143d0a7cb7dbb91e4e24e79db075975b6d3d4
Maxime DEFRETIN <maxime.defretin@laposte.net>
2017-12-13 08:57:32 +0100

    Correction

--- index.php
+++ index.php
@@ -1,7 +1,17 @@
-<?php
-$add = 3 + 4;
-$multiplication = 5 * 20;
-$division = 45 / 5;
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="style.css">
+    <title>Partiee 1 : exercice 8</title>
+  </head>
+  <body>
+    <?php
+    $add = 3 + 4;
+    $multiplication = 5 * 20;
+    $division = 45 / 5;
 
-echo "$add $multiplication $division";
-?>
+    echo $add . ' ' . $multiplication . ' ' . $division;
+    ?>
+  </body>
+</html>
